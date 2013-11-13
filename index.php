69e9592846ef32acf9f190a7f4af2b95afbe73a9
shahid <mshahid85@gmail.com>
2013-11-13 11:08:29 +0530

    added list of recipes

--- index.php
+++ index.php
@@ -2,5 +2,12 @@
 
 <h2>This is the list of recipes</h2>
 
+<ul>
+<li>How to make Burger?</li>
+<li>How to make Pizza?</li>
+<li>How to make Rice?</li>
+<li>How to make Curry?</li>
+</ul>
+
 <br />
 ----------------------------------
